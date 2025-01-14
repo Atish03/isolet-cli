2bd7dd99d5d160b5f92a6d5e08d9238b22801ea2
Atish03 <atishpersonal003@gmail.com>
2025-01-14 08:48:34 +0530

    Added client package

---
+++ client/client.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	"flag"
+	"path/filepath"
+)
+
+type CustomClient struct {
+	*kubernetes.Clientset
+}
+
+func GetClient() (clientset CustomClient) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	
+	clientset = CustomClient{client}
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return
+}
---
+++ client/secret.go
@@ -0,0 +1,18 @@
+package client
+
+import (
+	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (client CustomClient) GetDockerConfig(namespace string, name string) (configjson string, err error) {
+	
+	item, err := client.CoreV1().Secrets(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	configjson = string(item.Data[".dockerconfigjson"])
+
+	return
+}
