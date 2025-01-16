c44709820b1248c0a5f1076df3da9dc99337e060
Atish03 <atishpersonal003@gmail.com>
2025-01-16 14:47:40 +0530

    Added cp package

---
+++ cp/cp.go
@@ -0,0 +1,263 @@
+package cp
+
+import (
+	"archive/tar"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/kubectl/pkg/cmd/exec"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+)
+
+type CopyOptions struct {
+	Container  string
+	Namespace  string
+	NoPreserve bool
+	MaxTries   int
+
+	ClientConfig      *restclient.Config
+	Clientset         kubernetes.Interface
+	ExecParentCmdName string
+
+	Args []string
+
+	genericiooptions.IOStreams
+}
+
+var (
+	errFileSpecDoesntMatchFormat = errors.New("filespec must match the canonical format: [[namespace/]pod:]file/path")
+)
+
+func extractFileSpec(arg string) (fileSpec, error) {
+	i := strings.Index(arg, ":")
+
+	if i == -1 {
+		return fileSpec{
+			File: newLocalPath(arg),
+		}, nil
+	}
+
+	pod, file := arg[:i], arg[i+1:]
+	pieces := strings.Split(pod, "/")
+	switch len(pieces) {
+	case 1:
+		return fileSpec{
+			PodName: pieces[0],
+			File:    newRemotePath(file),
+		}, nil
+	case 2:
+		return fileSpec{
+			PodNamespace: pieces[0],
+			PodName:      pieces[1],
+			File:         newRemotePath(file),
+		}, nil
+	default:
+		return fileSpec{}, errFileSpecDoesntMatchFormat
+	}
+}
+
+func (o *CopyOptions) Run() error {
+	srcSpec, err := extractFileSpec(o.Args[0])
+	if err != nil {
+		return err
+	}
+	destSpec, err := extractFileSpec(o.Args[1])
+	if err != nil {
+		return err
+	}
+
+	return o.copyToPod(srcSpec, destSpec, &exec.ExecOptions{})
+}
+
+func (o *CopyOptions) copyToPod(src, dest fileSpec, options *exec.ExecOptions) error {
+	if _, err := os.Stat(src.File.String()); err != nil {
+		return fmt.Errorf("%s doesn't exist in local filesystem", src.File)
+	}
+	reader, writer := io.Pipe()
+
+	srcFile := src.File.(localPath)
+	destFile := dest.File.(remotePath)
+
+	go func(src localPath, dest remotePath, writer io.WriteCloser) {
+		defer writer.Close()
+		cmdutil.CheckErr(makeTar(src, dest, writer))
+	}(srcFile, destFile, writer)
+	var cmdArr []string
+
+	if o.NoPreserve {
+		cmdArr = []string{"tar", "--no-same-permissions", "--no-same-owner", "-xmf", "-"}
+	} else {
+		cmdArr = []string{"tar", "-xmf", "-"}
+	}
+	destFileDir := destFile.Dir().String()
+	if len(destFileDir) > 0 {
+		cmdArr = append(cmdArr, "-C", destFileDir)
+	}
+
+	options.StreamOptions = exec.StreamOptions{
+		IOStreams: genericiooptions.IOStreams{
+			In:     reader,
+			Out:    o.Out,
+			ErrOut: o.ErrOut,
+		},
+		Stdin: true,
+
+		Namespace: dest.PodNamespace,
+		PodName:   dest.PodName,
+	}
+
+	options.Command = cmdArr
+	options.Executor = &exec.DefaultRemoteExecutor{}
+
+	return o.execute(options)
+}
+
+type TarPipe struct {
+	src       fileSpec
+	o         *CopyOptions
+	reader    *io.PipeReader
+	outStream *io.PipeWriter
+	bytesRead uint64
+	retries   int
+}
+
+func (t *TarPipe) initReadFrom(n uint64) {
+	t.reader, t.outStream = io.Pipe()
+	options := &exec.ExecOptions{
+		StreamOptions: exec.StreamOptions{
+			IOStreams: genericiooptions.IOStreams{
+				In:     nil,
+				Out:    t.outStream,
+				ErrOut: t.o.Out,
+			},
+
+			Namespace: t.src.PodNamespace,
+			PodName:   t.src.PodName,
+		},
+
+		Command:  []string{"tar", "cf", "-", t.src.File.String()},
+		Executor: &exec.DefaultRemoteExecutor{},
+	}
+	if t.o.MaxTries != 0 {
+		options.Command = []string{"sh", "-c", fmt.Sprintf("tar cf - %s | tail -c+%d", t.src.File, n)}
+	}
+
+	go func() {
+		defer t.outStream.Close()
+		cmdutil.CheckErr(t.o.execute(options))
+	}()
+}
+
+func (t *TarPipe) Read(p []byte) (n int, err error) {
+	n, err = t.reader.Read(p)
+	if err != nil {
+		if t.o.MaxTries < 0 || t.retries < t.o.MaxTries {
+			t.retries++
+			fmt.Printf("Resuming copy at %d bytes, retry %d/%d\n", t.bytesRead, t.retries, t.o.MaxTries)
+			t.initReadFrom(t.bytesRead + 1)
+			err = nil
+		} else {
+			fmt.Printf("Dropping out copy after %d retries\n", t.retries)
+		}
+	} else {
+		t.bytesRead += uint64(n)
+	}
+	return
+}
+
+func makeTar(src localPath, dest remotePath, writer io.Writer) error {
+	tarWriter := tar.NewWriter(writer)
+	defer tarWriter.Close()
+
+	srcPath := src.Clean()
+	destPath := dest.Clean()
+	return recursiveTar(srcPath.Dir(), srcPath.Base(), destPath.Dir(), destPath.Base(), tarWriter)
+}
+
+func recursiveTar(srcDir, srcFile localPath, destDir, destFile remotePath, tw *tar.Writer) error {
+	matchedPaths, err := srcDir.Join(srcFile).Glob()
+	if err != nil {
+		return err
+	}
+	for _, fpath := range matchedPaths {
+		stat, err := os.Lstat(fpath)
+		if err != nil {
+			return err
+		}
+		if stat.IsDir() {
+			files, err := os.ReadDir(fpath)
+			if err != nil {
+				return err
+			}
+			if len(files) == 0 {
+				hdr, _ := tar.FileInfoHeader(stat, fpath)
+				hdr.Name = destFile.String()
+				if err := tw.WriteHeader(hdr); err != nil {
+					return err
+				}
+			}
+			for _, f := range files {
+				if err := recursiveTar(srcDir, srcFile.Join(newLocalPath(f.Name())),
+					destDir, destFile.Join(newRemotePath(f.Name())), tw); err != nil {
+					return err
+				}
+			}
+			return nil
+		} else if stat.Mode()&os.ModeSymlink != 0 {
+			hdr, _ := tar.FileInfoHeader(stat, fpath)
+			target, err := os.Readlink(fpath)
+			if err != nil {
+				return err
+			}
+
+			hdr.Linkname = target
+			hdr.Name = destFile.String()
+			if err := tw.WriteHeader(hdr); err != nil {
+				return err
+			}
+		} else {
+			hdr, err := tar.FileInfoHeader(stat, fpath)
+			if err != nil {
+				return err
+			}
+			hdr.Name = destFile.String()
+
+			if err := tw.WriteHeader(hdr); err != nil {
+				return err
+			}
+
+			f, err := os.Open(fpath)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err := io.Copy(tw, f); err != nil {
+				return err
+			}
+			return f.Close()
+		}
+	}
+	return nil
+}
+
+func (o *CopyOptions) execute(options *exec.ExecOptions) error {
+	if len(options.Namespace) == 0 {
+		options.Namespace = o.Namespace
+	}
+
+	if len(o.Container) > 0 {
+		options.ContainerName = o.Container
+	}
+
+	options.Config = o.ClientConfig
+	options.PodClient = o.Clientset.CoreV1()
+
+	return options.Run()
+}
