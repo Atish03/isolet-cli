a3a2e4f7998ae0fa9755075e65b3a72461a36be3
Atish03 <atishpersonal003@gmail.com>
2025-01-21 14:55:55 +0530

    Added logger package

---
+++ logger/logger.go
@@ -0,0 +1,37 @@
+package logger
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var (
+	infoStyle  = color.New(color.FgGreen, color.Bold).SprintfFunc()
+	errorStyle = color.New(color.FgRed, color.Bold).SprintfFunc()
+	debugStyle = color.New(color.FgBlue).SprintfFunc()
+	textStyle  = color.RGB(175, 175, 175).SprintFunc()
+)
+
+func LogMessage(level string, message string, prefix ...string) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	level = strings.ToUpper(level)
+
+	var coloredLevel string
+	switch level {
+	case "INFO":
+		coloredLevel = infoStyle("[INFO]")
+	case "ERROR":
+		coloredLevel = errorStyle("[ERROR]")
+	case "DEBUG":
+		coloredLevel = debugStyle("[DEBUG]")
+	default:
+		coloredLevel = fmt.Sprintf("[%s]", level)
+	}
+
+	prefixString := strings.Join(prefix, "][")
+
+	fmt.Printf("[%s]%s %s %s\n", prefixString, coloredLevel, timestamp, textStyle(message))
+}
