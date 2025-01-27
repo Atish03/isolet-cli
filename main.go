2a159c8e6e3d86335f607a490b389b2a16b2d72d
Atish03 <atishpersonal003@gmail.com>
2025-01-27 15:47:29 +0530

    Removed comments

--- main.go
+++ main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	// "fmt"
-	// "sync"
-
-	// "github.com/Atish03/isolet-cli/challenge"
-	// "github.com/Atish03/isolet-cli/client"
-	// "github.com/Atish03/isolet-cli/logger"
 	"github.com/Atish03/isolet-cli/cmd"
 )
 
 func main() {
-	// TO BE IMPLEMENTED
-	// kubecli, err := client.GetClient()
-	// if err != nil {
-	// 	logger.LogMessage("ERROR", fmt.Sprintf("cannot get client: %v", err), "Main")
-	// }
-
-	// challs := challenge.GetChalls("./test/sample_challs/")
-
-	// var wg sync.WaitGroup
-	
-	// for _, chall := range(challs) {
-	// 	wg.Add(1)
-		
-	// 	go func(){
-	// 		err := chall.Load(&kubecli, "automation", "docker.io/b3gul4", "https://index.docker.io/v1/", &wg)
-	// 		if err != nil {
-	// 			logger.LogMessage("ERROR", fmt.Sprintf("error loading challenge: %v", err), "Main")
-	// 		}
-	// 	}()
-	// }
-
-	// wg.Wait()
-
 	cmd.Execute()
 }
